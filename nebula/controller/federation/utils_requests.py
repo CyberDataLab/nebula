ac5aa53acbbfe241fdbe63fc120fdb24fb75d83c
FerTV <fernando.torres.vega@gmail.com>
2025-10-02 11:52:24 +0200

    fix: remove route typo

--- nebula/controller/federation/utils_requests.py
+++ nebula/controller/federation/utils_requests.py
@@ -5,25 +5,25 @@ class RunScenarioRequest(BaseModel):
     scenario_data: Dict[str, Any]
     user: str
     federation_id: str
-    
+
 class StopScenarioRequest(BaseModel):
     experiment_type: str
     federation_id: str
-    
+
 class NodeUpdateRequest(BaseModel):
-    config: Dict[str, Any] = {}    
-    
+    config: Dict[str, Any] = {}
+
 class NodeDoneRequest(BaseModel):
     idx: int
     deployment: str
     name: str
     federation_id: str
-    
+
 class RemoveScenarioRequest(BaseModel):
     experiment_type: str
     user: str
     scenario_name: str
-    
+
 class Routes:
     INIT = "/init"
     RUN = "/scenarios/run"
@@ -31,12 +31,12 @@ class Routes:
     UPDATE = "/nodes/{federation_id}/update"
     DONE = "/nodes/{federation_id}/done"
     FINISH = "/scenarios/{federation_id}/finish"
-    REMOVE = "scenario/{federation_id}/remove"
-    
-    @classmethod 
-    def format(cls, route: str, **kwargs) -> str: 
+    REMOVE = "/scenario/{federation_id}/remove"
+
+    @classmethod
+    def format(cls, route: str, **kwargs) -> str:
         return getattr(cls, route).format(**kwargs)
-    
+
 def factory_requests(resource: str, **kwargs) -> str:
     try:
         return Routes.format(resource.upper(), **kwargs)
@@ -44,5 +44,3 @@ def factory_requests(resource: str, **kwargs) -> str:
         raise ValueError(f"Resource not found: {resource}")
     except KeyError as e:
         raise ValueError(f"Missing parameter for route '{resource}': {e}")
-    
-    
